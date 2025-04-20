386d3d8e7a0a715528808b3938f7ae47cfaa77a8
オノゲンキ <k024G0005@g.neec.ac.jp>
2025-04-21 01:31:26 +0900

    完成

--- GameScene.h
+++ GameScene.h
@@ -3,14 +3,30 @@
 
 // ゲームシーン
 class GameScene {
-
-	uint32_t textureHandle_ = 0;
-
-	KamataEngine::Sprite* sprite_ = nullptr;
-
-
-
 public:
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	//サウンドデータの読み込み
+	uint32_t soundDataHandle_ = 0;
+	//音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	//スプライト
+	KamataEngine::Sprite* sprite_ = nullptr;
+	// 3Dモデル
+	KamataEngine::Model* model_ = nullptr;
+	// ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+	// カメラ
+	KamataEngine::Camera camera_;
+
+	//デバックカメラの生成
+	KamataEngine::DebugCamera* debugCamera_ = nullptr; 
+	//ImGuiで値を入力する変数
+	float inputFloat3[3] = {0, 0, 0};
+
+	// デストラクタ
+	~GameScene();
+
 	// 初期化
 	void Initialize();
 
@@ -19,6 +35,4 @@ public:
 
 	// 描画
 	void Draw();
-
-	~GameScene();
 };
