ff24b7d0930c5b4091a425465531ac30c47906ca
オノゲンキ <k024G0005@g.neec.ac.jp>
2025-05-07 12:50:32 +0900

    ブロックを横に並んだ

--- GameScene.cpp
+++ GameScene.cpp
@@ -1,30 +1,131 @@
 #include "GameScene.h"
-
 using namespace KamataEngine;
+
+Matrix4x4 Multiply(const Matrix4x4& matrix1, const Matrix4x4& matrix2) {
+	Matrix4x4 result = {};
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+				result.m[i][j] += matrix1.m[i][k] * matrix2.m[k][j];
+			}
+		}
+	}
+
+	return result;
+}
+
+// 拡大縮小行列の作成
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+
+	Matrix4x4 result{scale.x, 0.0f, 0.0f, 0.0f, 0.0f, scale.y, 0.0f, 0.0f, 0.0f, 0.0f, scale.z, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+
+	return result;
+}
+
+// X軸回転行列の作成
+Matrix4x4 MakeRotateXMatrix(float theta) {
+	float sin = std::sin(theta);
+	float cos = std::cos(theta);
+
+	Matrix4x4 result{1.0f, 0.0f, 0.0f, 0.0f, 0.0f, cos, sin, 0.0f, 0.0f, -sin, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+
+	return result;
+}
+
+// Y軸回転行列の作成
+Matrix4x4 MakeRotateYMatrix(float theta) {
+	float sin = std::sin(theta);
+	float cos = std::cos(theta);
+
+	Matrix4x4 result{cos, 0.0f, -sin, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, sin, 0.0f, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+
+	return result;
+}
+
+// Z軸回転行列の作成
+Matrix4x4 MakeRotateZMatrix(float theta) {
+	float sin = std::sin(theta);
+	float cos = std::cos(theta);
+
+	Matrix4x4 result{cos, sin, 0.0f, 0.0f, -sin, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+
+	return result;
+}
+
+// 平行移動行列の作成
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 result{1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, translate.x, translate.y, translate.z, 1.0f};
+
+	return result;
+}
+
+// 拡大縮小・回転・平行移動行列を使ってアフィン変換行列を作る関数
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate) {
+
+	// スケーリング行列の作成
+	Matrix4x4 matScale = MakeScaleMatrix(scale);
+
+	Matrix4x4 matRotX = MakeRotateXMatrix(rot.x);
+	Matrix4x4 matRotY = MakeRotateYMatrix(rot.y);
+	Matrix4x4 matRotZ = MakeRotateZMatrix(rot.z);
+	// 回転行列の合成
+	Matrix4x4 matRot = Multiply(Multiply(matRotX, matRotY), matRotZ);
+
+	// 平行移動行列の作成
+	Matrix4x4 matTrans = MakeTranslateMatrix(translate);
+
+	// スケーリング、回転、平行移動の合成
+	Matrix4x4 matTransform = Multiply(Multiply(matScale, matRot), matTrans);
+
+	return matTransform;
+}
 
 void GameScene::Initialize() {
 	// ファイル名を指定してテクスチャを読み込み
-	textureHandle_ = TextureManager::Load("sample.png");
+	/*textureHandle_ = TextureManager::Load("sample.png");*/
+
+		// 3Dモデルの生成
+	block_model_ = Model::Create();
 
 	// スプライトインスタンスの生成
-	model_ = Model::Create();
+	/*model_ = Model::Create();*/
 
 	//ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
+	/*worldTransform_.Initialize();*/
 	//カメラの初期化
 	camera_.Initialize();
 	
-	//自キャラの生成
-	player_ = new Player();
-	//自キャラの初期化
-	player_->Initialize(model_,textureHandle_,&camera_);
+	//ブロックモデル
+	/*blockModel_ = Model::Create();*/
+
+	// 要素数
+	const uint32_t kNumBlockHorizontal = 20;
+	// ブロック1個分の横幅
+	const float kBlockWidth = 2.0f;
+	// 要素数を変更する
+	worldTransformBlocks_.resize(kNumBlockHorizontal);
+	// キューブの作成
+	for (uint32_t i = 0; i < kNumBlockHorizontal; ++i) {
+		worldTransformBlocks_[i] = new WorldTransform();
+		worldTransformBlocks_[i]->Initialize();
+		worldTransformBlocks_[i]->translation_.x = kBlockWidth * i;
+		worldTransformBlocks_[i]->translation_.y = 0.0f;
+	}
 
 }
 
 void GameScene::Update() {
-//自キャラの更新
-	player_->Update();
+	// ブロックの更新
+	for (WorldTransform* worldTransformBlock : worldTransformBlocks_) {
 
+		// 拡大縮小・回転・平行移動行列を使ってアフィン変換行列を作る関数
+		Matrix4x4 affin = MakeAffineMatrix(worldTransformBlock->scale_, worldTransformBlock->rotation_, worldTransformBlock->translation_);
+
+		worldTransformBlock->matWorld_ = affin;
+		// 定数バッファに転送する
+		worldTransformBlock->TransferMatrix();
+	}
 }
 
 void GameScene::Draw() {
@@ -35,13 +136,18 @@ void GameScene::Draw() {
 	// 3Dオブジェクト描画前処理
 	Model::PreDraw(dxCommon->GetCommandList());
 
-	// 自キャラの描画
-	player_->Draw();
+	//ブロックの描画
+	for (WorldTransform* worldTransformBlock : worldTransformBlocks_) {
+		block_model_->Draw(*worldTransformBlock, camera_);
+	}
 
 	Model::PostDraw();
 }
 
 GameScene::~GameScene() {
-	delete model_;
-	delete player_;
+	delete block_model_;
+	for (WorldTransform* worldTransformBlock : worldTransformBlocks_) {
+		delete worldTransformBlock;
+	}
+	worldTransformBlocks_.clear();
 }
--- GameScene.h
+++ GameScene.h
@@ -1,23 +1,29 @@
 #pragma once
 #include "KamataEngine.h"
-#include "Player.h"
+#include<vector>
 
 // ゲームシーン
 class GameScene {
-public:
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0;
 
-	KamataEngine::Model* model_ = nullptr;
+private:
+	//3Dモデル
+	/*KamataEngine::Model* blockModel_ = nullptr;*/
+
+	//テクスチャハンドル
+	/*uint32_t textureHandle_ = 0;*/
+
+	/*KamataEngine::Model* model_ = nullptr;*/
 
 	//ワールドトランスフォーム
-	KamataEngine::WorldTransform worldTransform_;
+	/*KamataEngine::WorldTransform worldTransform_;*/
 	//カメラ
 	KamataEngine::Camera camera_;
 
-	//自キャラ
-	Player* player_ = nullptr;
+	//ブロックモデル
+	KamataEngine::Model* block_model_ = nullptr;
+	std::vector<KamataEngine::WorldTransform*> worldTransformBlocks_;
 
+public:
 	// 初期化
 	void Initialize();
 
