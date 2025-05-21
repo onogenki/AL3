dfd72b0957a417b2ac1651e5a53fbc14ab8d7a24
オノゲンキ <k024G0005@g.neec.ac.jp>
2025-05-21 13:55:51 +0900

    player処理完了

--- Player.cpp
+++ Player.cpp
@@ -1,59 +1,132 @@
+#define NOMINMAX
+
 #include "Player.h"
-#include<numbers>
-#include<algorithm>
+#include "Math.h"
+#include <algorithm>
+#include <cassert>
+#include <numbers>
 
-using namespace KamataEngine;
+void Player::Initialize(Model* model, Camera* camera, const Vector3& position) {
 
-void Player::Initialize(Model* model, Camera* camera) {
 	assert(model);
-
+	// モデル
 	model_ = model;
-	camera_ = camera;
+
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
 	worldTransform_.rotation_.y = std::numbers::pi_v<float> / 2.0f;
+
+	camera_ = camera;
 }
 
-void Player::Update() { Matrix4x4 affin_mat = MakeAffineMatrix(
-	worldTransform_.scale_,
-	worldTransform_.rotation_,
-	worldTransform_.translation_
-);
+void Player ::Update() {
 
-worldTransform.matWorld_ = affin_mat;
+	// 移動入力
+	if (onGround_) {
+		if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
 
-//移動入力
-//左右移動操作
-if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
-	// 左右加速
-	Vector3 acceleration = {};
-	if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
-		//左移動中の右入力
-		if (velocity_.x < 0.0f)
-		{
+			// 左右加速
+			Vector3 acceleration = {};
+			if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
+
+				// 左移動中の右入力
+				if (velocity_.x < 0.0f) {
+					// 速度と逆方向に入力中は急ブレーキ
+					velocity_.x *= (1.0f - kAttenuation);
+				}
+
+				acceleration.x += kAcceleration;
+
+				if (lrDirection_ != LRDirection::kRight) {
+					lrDirection_ = LRDirection::kRight;
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
+			} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+				// 右移動中の左入力
+				if (velocity_.x > 0.0f) {
+					// 速度と逆方向に入力中は急ブレーキ
+					velocity_.x *= (1.0f - kAttenuation);
+				}
+
+				acceleration.x -= kAcceleration;
+
+				if (lrDirection_ != LRDirection::kLeft) {
+					lrDirection_ = LRDirection::kLeft;
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
+			}
+
+			// 加速/減速
+			velocity_ += acceleration;
+			// 最大速度制限
+			velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+
+		} else {
+			// 非入力時は移動減衰をかける
 			velocity_.x *= (1.0f - kAttenuation);
 		}
-		acceleration.x = kAcceleration;
-	} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
-		if (velocity_.x > 0.0f) {
-			// 速度と逆方向に入力中は急ブレーキ
-			velocity_.x *= (1.0f - kAttenuation);
+
+		if (Input::GetInstance()->PushKey(DIK_UP)) {
+			// ジャンプ初速
+			velocity_ += Vector3(0, kJumpAcceleration / 60.0f, 0);
 		}
-		acceleration.x -= kAcceleration;
-	} else {
-		velocity_.x *= (1.0f - kAttenuation);
 	}
-	velocity_ += acceleration;
-}
-	//移動
-worldTransform_.translation_ += velocity_;
+	// 空中
+	else {
+		// 落下速度
+		velocity_ += Vector3(0, -kGravityAcceleration / 60.0f, 0);
+		// 落下速度制限
+		velocity_.y = std::max(velocity_.y, -kLimitFallSpeed);
+	}
 
-//行列更新
+	worldTransform_.translation_ += velocity_;
 
+	bool landing = false;
 
+	// 下降あり？
+	if (velocity_.y < 0) {
+		// Y座標が地面以下になったら着地
+		if (worldTransform_.translation_.y <= 1.0f) {
+			landing = true;
+		}
+	}
+
+	// 接地判定
+	if (onGround_) {
+		// ジャンプ開始
+		if (velocity_.y > 0.0f) {
+			onGround_ = false;
+		}
+	} else {
+		// 着地
+		if (landing) {
+			worldTransform_.translation_.y = 1.0f;
+			velocity_.x *= (1.0f - kAttenuation);
+			velocity_.y = 0.0f;
+			onGround_ = true;
+		}
+	}
+
+	// 旋回制御
+	if (turnTimer_ > 0.0f) {
+		// タイマーを進める
+		turnTimer_ = std::max(turnTimer_ - (1.0f / 60.0f), 0.0f);
+
+		float destinationRotationYTable[] = {std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> * 3.0f / 2.0f};
+
+		float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
+
+		worldTransform_.rotation_.y = EaseInOut(destinationRotationY, turnFirstRotationY_, turnTimer_ / kTimeTurn);
+	}
+
+	// ワールド行列更新（アフィン変換～DirectXに転送）
+	WorldTransformUpdate(worldTransform_);
 }
 
 void Player::Draw() {
+
 	// モデル描画
 	model_->Draw(worldTransform_, *camera_);
 }
