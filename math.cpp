93fa9a72fa4babae8a7a7e0110d96f32f5cea57f
オノゲンキ <k024G0005@g.neec.ac.jp>
2025-08-12 11:07:30 +0900

    一応変更

--- math.cpp
+++ math.cpp
@@ -1,6 +1,25 @@
+#include "math.h"
 #include <cmath>
 #include <numbers>
-#include "math.h"
+
+// 0214 単項演算子オーバーロード
+Vector3 operator+(const Vector3& v) { return v; }
+Vector3 operator-(const Vector3& v) { return Vector3(-v.x, -v.y, -v.z); }
+
+// CameraControllerのUpdateで必要
+const Vector3 operator*(const Vector3& v1, const float f) {
+	Vector3 temp(v1);
+	return temp *= f;
+}
+
+// CameraControllerのUpdate/Reset関数で必要
+const Vector3 operator+(const Vector3& v1, const Vector3& v2) {
+	Vector3 temp(v1);
+	return temp += v2;
+}
+
+// Lerp関数
+Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t) { return Vector3(Lerp(v1.x, v2.x, t), Lerp(v1.y, v2.y, t), Lerp(v1.z, v2.z, t)); }
 
 // Velocity_を使えるように
 Vector3& operator+=(Vector3& lhv, const Vector3& rhv) {
@@ -31,17 +50,8 @@ Vector3& operator/=(Vector3& v, float s) {
 	return v;
 }
 
-Matrix4x4 Multiply(const Matrix4x4& matrix1, const Matrix4x4& matrix2) {
-
-	Matrix4x4 result = {};
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			for (int k = 0; k < 4; k++) {
-				result.m[i][j] += matrix1.m[i][k] * matrix2.m[k][j];
-			}
-		}
-	}
+Matrix4x4 MakeIdentityMatrix() {
+	static const Matrix4x4 result{1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
 
 	return result;
 }
@@ -101,19 +111,60 @@ Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vecto
 	Matrix4x4 matRotY = MakeRotateYMatrix(rot.y);
 	Matrix4x4 matRotZ = MakeRotateZMatrix(rot.z);
 	// 回転行列の合成
-	Matrix4x4 matRot = Multiply(Multiply(matRotX, matRotY), matRotZ);
+	Matrix4x4 matRot = matRotZ * matRotX * matRotY;
 
 	// 平行移動行列の作成
 	Matrix4x4 matTrans = MakeTranslateMatrix(translate);
 
 	// スケーリング、回転、平行移動の合成
-	Matrix4x4 matTransform = Multiply(Multiply(matScale, matRot), matTrans);
+	Matrix4x4 matTransform = matScale * matRot * matTrans;
 
 	return matTransform;
 }
 
-//EaseInOutで必要
+Matrix4x4& operator*=(Matrix4x4& lhm, const Matrix4x4& rhm) {
+	Matrix4x4 result{};
+
+	for (size_t i = 0; i < 4; i++) {
+		for (size_t j = 0; j < 4; j++) {
+			for (size_t k = 0; k < 4; k++) {
+				result.m[i][j] += lhm.m[i][k] * rhm.m[k][j];
+			}
+		}
+	}
+	lhm = result;
+	return lhm;
+}
+
+Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result = m1;
+
+	return result *= m2;
+}
+
+void WorldTransformUpdate(WorldTransform& worldTransform) {
+
+	Matrix4x4 affin_mat = MakeAffineMatrix(worldTransform.scale_, worldTransform.rotation_, worldTransform.translation_);
+
+	worldTransform.matWorld_ = affin_mat;
+
+	worldTransform.TransferMatrix();
+}
+
+// EaseInOutで必要
 float Lerp(float x1, float x2, float t) { return (1.0f - t) * x1 + t * x2; }
+
+float EaseIn(float x1, float x2, float t) {
+	float easedT = t * t;
+
+	return Lerp(x1, x2, easedT);
+}
+
+float EaseOut(float x1, float x2, float t) {
+	float easedT = 1.0f - std::powf(1.0f - t, 3.0f);
+
+	return Lerp(x1, x2, easedT);
+}
 
 float EaseInOut(float x1, float x2, float t) {
 	float easedT = -(std::cosf(std::numbers::pi_v<float> * t) - 1.0f) / 2.0f;
@@ -121,10 +172,22 @@ float EaseInOut(float x1, float x2, float t) {
 	return Lerp(x1, x2, easedT);
 }
 
-void worldTransformUpdate(WorldTransform& worldTransform) {
-
-	worldTransform.matWorld_ = MakeAffineMatrix(worldTransform.scale_, worldTransform.rotation_, worldTransform.translation_);
-
-	worldTransform.TransferMatrix();
+bool IsCollision(const AABB& aabb1, const AABB& aabb2) {
+	return (aabb1.min.x <= aabb2.max.x && aabb1.max.x >= aabb2.min.x) && // x軸
+	       (aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y) && // y軸
+	       (aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z);   // z軸
 }
 
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
+	Vector3 result; // w=1がデカルト座標系であるので(x,y,1)のベクトルとしてmatrixとの積をとる
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w != 0.0f); // ベクトルに対して基本的な操作を行う行列でwが0になることはありえない
+	// w=1がデカルト座標系であるので、w除算することで同次座標をデカルト座標に戻す
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
+}
